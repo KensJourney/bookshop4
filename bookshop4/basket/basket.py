716452493cf24608323fd2ddd502c77d370668ed
KensJourney <78187687+KensJourney@users.noreply.github.com>
2022-02-05 14:22:02 +0600

    Initial commit

---
+++ bookshop4/basket/basket.py
@@ -0,0 +1,45 @@
+from store.models import Product
+
+
+class Basket():
+
+
+    def __init__(self, request):
+        self.session = request.session
+        basket = self.session.get('skey')
+        if 'skey' not in request.session:
+            basket = self.session['skey'] = {}
+        self.basket = basket
+
+    def add(self, product, qty):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = str(product.id)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
+
+        self.save()
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
+
+    def save(self):
+        self.session.modified = True
+
+
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database
+        and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
